ab5d8de37dd9eb77c74e01745e9be912a1f6c94b
SM-Oslomet <samez5302@oslomet.no>
2025-09-20 23:58:57 +0200

    added launch and tasks json files to vscode. These are for debugging.

--- obj/Debug/net9.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57e5dc8cdc8b631c64b415955a628b7b13f7a67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6eb1db772fbfd2b7eb9687852ca4fbf75571eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
