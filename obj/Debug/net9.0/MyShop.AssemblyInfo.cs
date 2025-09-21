cb51c612d5900cc78cc154c9d6d7b89a8aa5af7a
SM-Oslomet <samez5302@oslomet.no>
2025-09-21 03:06:37 +0200

    added new stuff

--- obj/Debug/net9.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5d8de37dd9eb77c74e01745e9be912a1f6c94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0066905b9dcb0b434d66b52eb152c54f52c910f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
