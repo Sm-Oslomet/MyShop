283213662b79c09cc09f53365c294a038caf72d8
SM-Oslomet <samez5302@oslomet.no>
2025-10-07 15:46:30 +0200

    added database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Myshop.models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); // adds services required for handling controllers and views to teh dependency injection container. Setsupp application to use the MVC pattern for handling http requests
+
+builder.Services.AddDbContext<ItemDbContext>(options =>{
+    options.UseSqlite(
+        builder.Configuration["ConnectionStrings:ItemDbContextConnection"]);
+});
+
 
 var appa = builder.Build();
 
